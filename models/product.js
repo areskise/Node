4bc8951590716447aa04469c06e6de4d2b18f1a3
Ares Kise <areskise@gmail.com>
2022-11-09 21:35:26 +0700

    LAB08.1-Node

--- models/product.js
+++ models/product.js
@@ -27,15 +27,23 @@ module.exports = class Product {
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log("error:", err);
+                console.log("Product error:", err);
             });
         });
     }
 
     static fetchAll(cb) {
         getProductsFromFile(cb);
+    }
+
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+          const product = products.find(p => p.id === id);
+          cb(product);
+        });
     }
 }
--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const cors = require('cors');
 const app = express();
 
 const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
 
 app.use(cors());
 app.use(express.json());
 
 app.use(productRoutes);
+app.use(cartRoutes);
 
 app.listen(5000);
